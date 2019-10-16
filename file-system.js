cbf013869265ebcd2374acf43f42cf792e6e1be5
Amit Joseph <amitbj96@yahoo.com>
2019-10-16 16:10:07 +0530

    .

---
+++ file-system.js
@@ -0,0 +1,16 @@
+/*
+
+Module for File System is "fs"
+
+=> READING a File
+fs.readFile( fileName , Error Handle)
+
+=> CREATING a File
+fs.appendFile()
+fs.open()
+fs.writeFile()
+
+*/ 
+
+var fs = require('fs');
+
